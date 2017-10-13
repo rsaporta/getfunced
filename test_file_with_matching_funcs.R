eca236ebc271222b5d2157f1ed6a3d5d5856c429
Rick Saporta <rick.saporta@vydia.com>
2017-10-13 16:50:20 -0400

    Another Test file

---
+++ test_file_with_matching_funcs.R
@@ -0,0 +1,36 @@
+# test_file_with_matching_funcs.R
+
+
+function_one <- function(file_full_path, filename="name 1", folder=getOption("wrkDir", default="~/test_dir"), unqiue_param, wrongly_shared= list(1, LETTERS, "ac")) {
+  ### Comments
+  "STUFF"
+
+  ## more comments
+  inner_function <- function(this_should_not_be_used) {
+    "more stuff"
+  }
+
+  ## more more comments
+  "more more stuff"
+}
+
+
+
+function_two <- function(file_full_path, filename="NAME 2", folder=getOption("wrkDir", default="~/test_dir"), unqiue_param_number_2, wrongly_shared="some param") {
+  "bar"
+
+  list(foo = "bar")
+
+  ### Comments
+  "STUFF"
+
+  ## more comments
+  inner_function <- function(this_should_not_be_used) {
+    "more stuff"
+  }
+
+  ## more more comments
+  "more more stuff"
+}
+
+
